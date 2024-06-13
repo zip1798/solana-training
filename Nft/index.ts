66dd90a130a804497c59b14908083849549ee77f
Oleksandr Tsivun <067x5016783@gmail.com>
2024-06-13 23:53:28 +0300

    update, add collection to nft

--- Nft/index.ts
+++ Nft/index.ts
@@ -9,6 +9,7 @@ import {
   Nft
 } from "@metaplex-foundation/js"
 import * as fs from "fs"
+import { syncNative } from "@solana/spl-token";
 
 const PATH = "Nft/";
 
@@ -65,13 +66,32 @@ async function main() {
         timeout: 60000,
         providerUrl: "https://api.devnet.solana.com",
       })
-    )
+  )
 
-   // upload the NFT data and get the URI for the metadata
-   const uri = await uploadMetadata(metaplex, nftData);
+  const CollectionNftData = {
+        name: "TestCollectionNFT",
+        symbol: "TEST",
+        description: "Test Collection NFT",
+        sellerFeeBasisPoints: 100,
+        imageFile: "success.png",
+        isCollection: true,
+        collectionAuthority: user
+  };
+      
+  const collectionUri = await uploadMetadata(metaplex, CollectionNftData);
+  const collectionNft = await createCollectionNft(metaplex, collectionUri, CollectionNftData);
+    
+
+  // upload the NFT data and get the URI for the metadata
+  const uri = await uploadMetadata(metaplex, nftData);
    
-   // create an NFT using helper function and the URI from the metadata
-   const nft = await createNft(metaplex, uri, nftData);
+  // create an NFT using helper function and the URI from the metadata
+  const nft = await createNft(metaplex, uri, nftData, collectionNft.mint.address);
+
+  const updtatedUri = await uploadMetadata(metaplex, updateNftData);
+  await updateNftUri(metaplex, updtatedUri, nft.address);
+
+
 }
 
 // -----------------------------------------------------------------
@@ -88,7 +108,7 @@ async function uploadMetadata(
 
     // upload image and get image uri
     const imageUri = await metaplex.storage().upload(file);
-    console.log(`1. ✅ Image URI: ${imageUri}`);
+    console.log(`*. ✅ Image URI: ${imageUri}`);
 
     // upload metadata and get metadata uri
     const { uri } = await metaplex
@@ -99,7 +119,7 @@ async function uploadMetadata(
             description: nftData.description,
             image: imageUri,
         });
-    console.log(`2. ✅ Metadata URI: ${uri}`);
+    console.log(`*. ✅ Metadata URI: ${uri}`);
     return uri
 }
 
@@ -109,6 +129,7 @@ async function createNft(
     metaplex: Metaplex,
     uri: string,
     nftData: NftData,
+    collectionMint: PublicKey
 ): Promise<Nft> {
 
     const { nft } = await metaplex
@@ -118,12 +139,61 @@ async function createNft(
             name: nftData.name,
             sellerFeeBasisPoints: nftData.sellerFeeBasisPoints,
             symbol: nftData.symbol,
+            collection: collectionMint,
         }, { commitment: 'finalized'});
+
+    await metaplex.nfts().verifyCollection({
+        mintAddress: nft.address,
+        collectionMintAddress: collectionMint,
+        isSizedCollection: true,
+    });    
 
     console.log(`3. ✅ Created new NFT: ${nft.address.toBase58()}; https://explorer.solana.com/address/${nft.address.toBase58()}?cluster=devnet`);
     return nft;
 
 }
+
+
+// -----------------------------------------------------------------
+// helper function update NFT
+async function updateNftUri(
+    metaplex: Metaplex,
+    uri: string,
+    mintAddress: PublicKey,
+) {
+    // fetch NFT data using mint address
+    const nft = await metaplex.nfts().findByMint({ mintAddress });
+
+    // update NFT metadata
+    const { response } = await metaplex.nfts().update({
+        nftOrSft: nft,
+        uri: uri,
+    }, { commitment: 'finalized' });
+    console.log(`4.1. Token Mint https://explorer.solana.com/address/${nft.address.toBase58()}?cluster=devnet`);
+    console.log(`4.2. Transaction: https://explorer.solana.com/address/${response.signature}?cluster=devnet`);
+}
+
+// -----------------------------------------------------------------
+// helper function create Collectin NFT
+async function createCollectionNft(
+    metaplex: Metaplex,
+    uri: string,
+    data: CollectionNftData,
+): Promise<Nft> {
+    const { nft } = await metaplex.nfts().create({
+        uri,
+        name: data.name,
+        sellerFeeBasisPoints: data.sellerFeeBasisPoints,
+        symbol: data.symbol,
+        isCollection: true,
+        }, { commitment: 'finalized'});
+
+    console.log(`5. ✅ Created new Collection NFT: ${nft.address.toBase58()}; https://explorer.solana.com/address/${nft.address.toBase58()}?cluster=devnet`);    
+
+    return nft;    
+}
+
+
 // ==================================================================
 
 main()
@@ -148,5 +218,20 @@ Current balance is 1.9228136
 2. ✅ Metadata URI: https://arweave.net/idC1If0W2a_IYf8t9YYz7PEtcVzK2r4TBVXxO6fZm-0
 3. ✅ Created new NFT: GQm2pDg3fSPauUfqAGhRoR1Huh3SvAautYi3sHnzfkUT; https://explorer.solana.com/address/GQm2pDg3fSPauUfqAGhRoR1Huh3SvAautYi3sHnzfkUT?cluster=devnet
 Finished successfully
+
+
+
+zip@pop-os:~/solana-training$ npx esrun ./Nft/index.ts 
+Current balance is 1.878394414
+0. PublicKey: B5EucqcybsSdvP2CYQzJwmKDmTLgPMWnY2Gswjv4LwNb
+1. ✅ Image URI: https://arweave.net/7NafmkQRLutE3JML6zTvVlyi6QhpT6mYTbLxDbHzRnA
+2. ✅ Metadata URI: https://arweave.net/OF3gottG6rMZ_lA7muh1OP1AoeYBh7Gsr7HUlPxH-Ro
+3. ✅ Created new NFT: 3V1xqmRKfsKvErxXe44WgXYc7Laxu1CXs1XmpHHKbek9; https://explorer.solana.com/address/3V1xqmRKfsKvErxXe44WgXYc7Laxu1CXs1XmpHHKbek9?cluster=devnet
+1. ✅ Image URI: https://arweave.net/RO-s3qR0V41t4SC62LnDC9EPtD2_kcnsuDt_3m3oXUg
+2. ✅ Metadata URI: https://arweave.net/kU2ua6ri7grs84MYw2NX1-VNBlsRP9kE-9s5K-Lenvw
+4.1. Token Mint https://explorer.solana.com/address/3V1xqmRKfsKvErxXe44WgXYc7Laxu1CXs1XmpHHKbek9?cluster=devnet
+4.2. Transaction: https://explorer.solana.com/address/3khQDsB6JQShymnzTXXLL3kLLdXn3VfdtKauchVEm7JUQNhbSrJGCDWquraSYRBqgKK3CgiP5zCagdjGCcESS9qp?cluster=devnet
+Finished successfully
+
    * 
    */
